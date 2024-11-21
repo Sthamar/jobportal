b29a504a819fad49281f8681b6d340accb988630
Amar <reficul2627@gmail.com>
2024-11-21 06:40:34 +0000

    some changes

--- jobapp/settings.py
+++ jobapp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-zku7nomx4tnj(g+^l-_4w^pq0+h&_s8pj_yxe!y%&1k*qrem0%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['amar2627.pythonanywhere.com']
 
 
 # Application definition
@@ -57,7 +58,7 @@ ROOT_URLCONF = 'jobapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR,'templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR/ 'static'
 
 MEDIA_ROOT = BASE_DIR/ 'uploads'
 MEDIA_URL = '/media/'
